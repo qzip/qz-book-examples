97147bd0f2375ab30be22c1cc4c35f75ce3637c1
innomon <tech@ashishbanerjee.com>
2024-02-25 17:06:16 +0530

    perms

--- erp/doctype.go
+++ erp/doctype.go
@@ -1,22 +1,22 @@
 package erp
 
+import "time"
+
 // DocDef is a struct that is inspired by the DocDef in ERPNext
 // It connects a JSON schema to a set of templates and rules
 // The schema, templates and rules are looked up from the registry.
 type DocDefCfg struct {
-	Name     string `json:"name"`
-	Schema   string `json:"schema"`
-	Template string `json:"template"`
-	Rule     string `json:"rule"`
+	Name       string `json:"name"`
+	Schema     string `json:"schema"`
+	Template   string `json:"template"`
+	Rule       string `json:"rule"`
+	DataAccess string `json:"dataAccess"`
 }
 type DocDef struct {
-	rules     Rule
-	schemas   Schema
-	templates Template
-}
-
-func NewDocDef(cfg DocDefCfg) *DocDef {
-
+	Name     string
+	rule     Rule
+	schema   Schema
+	template Template
 }
 
 type Rule interface {
@@ -30,3 +30,40 @@ type Schema interface {
 }
 
 type Template interface{}
+
+type Role struct {
+	Name        string   `json:"name"`
+	Permissions []string `json:"permissions"`
+}
+
+type Permission struct {
+	Name        string   `json:"name"`
+	FieldGroups []string `json:"fieldGroups"`
+}
+
+// FieldGroup is a struct that is inspired ERPNext permission system
+// empty fields means level applies to all fields
+type FieldGroup struct {
+	Name   string   `json:"name"`
+	DocDef string   `json:"docDef"`
+	Fields []string `json:"fields,omitempty"`
+	Level  int      `json:"level"`
+}
+
+// Caser CAS Storage Provider, from imle/caser.go
+type Caser interface {
+	Store(data HashableData, ns string) (cas CasAddress, tmstamp time.Time, err error)
+	Load(cas CasAddress) (data []byte, ns string, tmstamp time.Time, err error)
+}
+
+// HashableData must be implemnted for Caser storage
+type HashableData interface {
+	Hash() []byte
+	Value() []byte
+}
+type CasAddress []byte
+
+type DataAccessor interface {
+	Caser
+	Query(query string) (casAddresses []CasAddress, err error)
+}
