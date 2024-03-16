56014134557f266fd63210a27e34673f239b00ea
innomon <tech@ashishbanerjee.com>
2024-03-16 12:29:03 +0530

    sql cas

--- erp/cas-dolt.sql
+++ erp/cas-dolt.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `cas` (
+  `id` varchar(36) default(uuid()) primary key
   `w3cdid` varchar(512) NOT NULL,
   `namespace` varchar(512) DEFAULT ('one.vyapar.cas'),
   `cas_data` json,
   `tmstamp` timestamp,
-  PRIMARY KEY (`w3cdid`)
+   KEY (`w3cdid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin
 
--- erp/cas-sqlite.sql
+++ erp/cas-sqlite.sql
@@ -1,1 +1,8 @@
-CREATE TABLE "cas" ( "w3cdid" TEXT NOT NULL UNIQUE, "namespace" TEXT NOT NULL DEFAULT 'in.innomon.b2b', "cas_data" JSON, "tmstamp" DATETIME NOT NULL, PRIMARY KEY("w3cdid") )
+CREATE TABLE "cas" (
+	"id"	INTEGER NOT NULL,
+	"w3cdid"	TEXT NOT NULL,
+	"namespace"	TEXT NOT NULL DEFAULT 'in.innomon.b2b',
+	"cas_data"	JSON,
+	"tmstamp"	DATETIME NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)
