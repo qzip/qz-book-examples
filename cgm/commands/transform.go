bd4349304dc5d924483fad3dcdc0ca5837877a10
innomon <tech@ashishbanerjee.com>
2024-03-23 11:59:50 +0530

    cgm wip

--- cgm/commands/transform.go
+++ cgm/commands/transform.go
@@ -1,8 +1,15 @@
 package commands
 
 import (
+	"bufio"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3" // Import go-sqlite3 library
 )
 
 func init() {
@@ -40,7 +47,52 @@ func getParams(param interface{}) (cp *CSV2DbParam, err error) {
 	return
 }
 
+const stmt = "REPLACE INTO cgm (dtm, glucose, userid) VALUES(?,?,?)"
+
 func (cd *CSV2DbParam) Process() error {
+	db, err := sql.Open("sqlite3", cd.DbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	fileIn, err := os.Open(cd.CSVfile)
+	if err != nil {
+		return err
+	}
+	defer fileIn.Close()
+
+	pstmt, err := db.Prepare(stmt)
+	if err != nil {
+		return err
+	}
+
+	rd := bufio.NewReader(fileIn)
+	i := 1
+	for {
+
+		ln, err := rd.ReadString('\n')
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if err == io.EOF {
+			break
+		}
+		ln = strings.TrimSpace(ln)
+		if ln != "" {
+			if i > 2 {
+				row := strings.Split(ln, ",")
+				if len(row) < 2 {
+					return fmt.Errorf("CSV2DbParam.Process: expecting 2 column at %v row", i)
+				}
+				if _, err = pstmt.Exec(row[0], row[1], cd.UserId); err != nil {
+					return err
+				}
+			}
+			i++
+
+		}
+	}
 
 	return nil
 }
@@ -48,6 +100,18 @@ func (cd *CSV2DbParam) Process() error {
 const csv2dbHelp = `
  csv2db: Converts CSV to cgm database.
  Sqlite database is used.
+
+ CREATE TABLE "cgm" (
+	"dtm"	DATETIME NOT NULL,
+	"glucose"	INTEGER,
+	"userid"	TEXT,
+	PRIMARY KEY("dtm")
+)
+
+CREATE INDEX "uid" ON "cgm" (
+	"userid"	ASC
+)
+
 
 
 `
--- cgm/go.mod
+++ cgm/go.mod
@@ -1,3 +1,5 @@
 module cgm
 
 go 1.22.0
+
+require github.com/mattn/go-sqlite3 v1.14.22
